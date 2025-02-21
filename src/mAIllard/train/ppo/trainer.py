3a2543b9a3efab0cf7dc70a07fbfe2dbe1885f6d
Bunny <kxfan21@m.fudan.edu.cn>
2025-02-21 19:58:16 +0800

    trainer dict create

---
+++ src/mAIllard/train/ppo/trainer.py
@@ -0,0 +1,35 @@
+from trl import PPOTrainer, PPOConfig
+from transformers import Trainer
+
+class mAIllardPPOTrainer(PPOTrainer, Trainer):
+    """
+    Self-defined PPO trainer
+    """
+    def __init__(
+        self,
+        model_args,
+        trainer_args,
+        model,
+        reward_model,
+        ref_model,
+        tokenizer,
+        processor,
+        train_dataset,
+        eval_dataset
+    ):
+        ppo_config = PPOConfig(
+
+        )
+
+        optimizer = ""
+        scheduler = ""
+
+        PPOTrainer.__init__(
+            self,
+            PPOConfig=ppo_config,
+            model=model,
+            ref_model=ref_model,
+            reward_model=reward_model,
+            train_dataset=train_dataset,
+        )
+
